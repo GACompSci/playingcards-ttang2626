ab8a35ca5ae8e0ad3c8f761bd5281cbe5d08e80a
Tyler Tang <ttang26@germantownacademy.org>
2024-12-10 21:26:58 -0500

    submtiting this project

--- src/App.java
+++ src/App.java
@@ -2,5 +2,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Use nested loops to create a full deck (plus two Jokers)
         // printing each card as you create it. 
+
+        for(int i = 1; i <= 6; i++){
+            for(int n = 1; n <= 13; n++){
+                if(i <= 4){
+                    Card card = new Card(n, i);
+                    System.out.println(card.toString());
+                }
+            }
+            if (i > 4){
+                Card joker = new Card();
+                System.out.println(joker.toString());
+            }
+
+        }
     }
 }
